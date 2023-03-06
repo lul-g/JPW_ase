e88dc8e41f112ce658ee30c2527d6c0a9b83235b
Lulseged Admasu <lulsegedgirma10@gmail.com>
2023-03-06 16:38:38 -0500

    assn 2 (jpw) done

--- assets/js/detail_populate.js
+++ assets/js/detail_populate.js
@@ -1,7 +1,27 @@
 import data from '../../data.json' assert { type: "json" }
 import img from '../../pictures.json' assert { type: "json" }
 
-document.getElementById("cont").innerHTML += `
+function displayPet() {
+
+    console.log(getAllUrlParams())
+    if(getAllUrlParams().pet >= data.length || getAllUrlParams().pet < 0) {
+        document.body.innerHTML = 
+    `
+        <div class="err_flex">
+            <div class="text"> 
+                <h1>Whoops!</h1>
+                <p>Sorry the page you are looking for does not exist</p>
+            <a href="index.html?page=0"  type="button" class="back more btn btn-sm btn-outline-secondary">Back</a>
+    
+            </div>
+            <div class="img"> 
+                <img src="assets/img/err_dog.jpg" alt="">
+            </div>
+        </div>
+    `
+    return;
+    }
+    document.getElementById("cont").innerHTML += `
     <h1>${data[getAllUrlParams().pet].name}</h1>
     <div>${data[getAllUrlParams().pet].additional_details}</div>
     <div class="table">
@@ -18,26 +38,37 @@ document.getElementById("cont").innerHTML += `
     
     <hr />
     <h2>Image Gallery</h2>
-    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3">
+    <div class="row row-cols-1 row-cols-sm-2 row-cols-md-3 g-3" id="insert">
         <div class="col">
-        <div class="card shadow-sm">
-            <img src=${img[getAllUrlParams().pet]}>
+            <div class="card shadow-sm">
+                <img src=${img[getAllUrlParams().pet]}>
+            </div>
         </div>
-        </div>
-        <div class="col">
-        <div class="card shadow-sm">
-            <img class="det_img" src=${img[Math.floor(Math.random() * img.length) - 1]}>
-        </div>
-        </div>
-        <div class="col">
-        <div class="card shadow-sm">
-            <img src=${img[Math.floor(Math.random() * img.length) - 1]}>
-
-        </div>
-        </div>
-  </div>
-  <a href="index.html?page=${parseInt(getAllUrlParams().pet / 6)}"  type="button" class="back more btn btn-sm btn-outline-secondary">Back</a>
+    </div>
+    <a href="index.html?page=${parseInt(getAllUrlParams().pet / 6)}"  type="button" class="back more btn btn-sm btn-outline-secondary">Back</a>
 `   
-// if((isNaN(getAllUrlParams().pet ) || getAllUrlParams().pet == undefined) && getAllUrlParams()[0] == 'pet') {
-//     window.location.href = "index.html?pet=0"
-// }
+
+    for (let index = 0; index < Math.floor(Math.random() * 6 )+ 1; index++) {
+        let rand_file = img[Math.floor(Math.random() * img.length) - 1] 
+        document.querySelector("#insert").innerHTML += `  
+        <div class="col">
+            <div class="card shadow-sm">
+                <img class="det_img" src=${rand_file}>
+            </div>
+        </div>
+        `
+    }
+    if(data[getAllUrlParams().pet].gender.toLowerCase() == "male") {
+        document.body.classList.add('bg-secondary')
+        document.querySelector('.album').classList.remove('bg-light')
+
+
+    }
+    else {
+        document.body.classList.add('bg-primary')
+        document.querySelector('.album').classList.remove('bg-light')
+    }
+}
+
+displayPet()
+
--- assets/js/index_populate.js
+++ assets/js/index_populate.js
@@ -13,47 +13,69 @@ function populate(page) {
     end = page * 6 >= data.length - 6 ? data.length : start + 6
     console.log("start:", start, "end:", end)
 
+    let count = 0
     for(let i = start; i < end; i++) {
-        document.querySelector("#row").innerHTML += 
-        `
-        <div class="col">
-            <div class="card shadow-sm">
-                <div class="img">
-                    <img src=${img[i]} class="bd-placeholder-img card-img-top">
-                    <span class="name" data-name=${data[i].name}></span>
-                </div>                
+        if(data[i].adoptable == 'no') {
+            end = end + 1
+        }
+        else {
+            count++
+            document.querySelector("#row").innerHTML += 
+                `
+                <div class="col">
+                    <div class="card shadow-sm">
+                        <div class="img">
+                            <img src=${img[i]} class="bd-placeholder-img card-img-top">
+                            <span class="name" data-name=${data[i].name}></span>
+                        </div>                
 
-                <div class="card-body">
-                    <p class="card-text">
-                        <span class="info">Gender: ${data[i].gender}</span>
-                        <span class="info">Age: ${data[i].age}</span>
-
-                    </p>
-                    <div class="d-flex justify-content-between align-items-center">
-                    <div class="btn-group">
-                        <a href="detail.html?pet=${i}" type="button" class="view btn btn-sm btn-outline-secondary">View</a>
-                        
-                    </div>
-                    <small class="text-muted">Breed: ${data[i].breed}</small>
+                        <div class="card-body">
+                            <p class="card-text">
+                                <span class="info">Gender: ${data[i].gender}</span>
+                                <span class="info">Age: ${data[i].age}</span>
+                            </p>
+                            <div class="d-flex justify-content-between align-items-center">
+                            <div class="btn-group">
+                                <a href="detail.html?pet=${i}" type="button" class="view btn btn-sm btn-outline-secondary">View</a>
+                                
+                            </div>
+                            <small class="text-muted">Breed: ${data[i].breed}</small>
+                            </div>
+                        </div>
                     </div>
                 </div>
-            </div>
-        </div>
-        `
-    }
-    document.querySelector(".prev_nex").innerHTML += `
-        <a  href="index.html?page=${
-            parseInt(getAllUrlParams().page) == 0 ? 0 : parseInt(getAllUrlParams().page) - 1}" 
-            class="btn btn-sm btn-outline-secondary more" id="prev" onclick="set_start()">Previous</a>
-        <a  href="index.html?page=${
-            parseInt(getAllUrlParams().page) * 6 >= data.length - 6 ? getAllUrlParams().page : parseInt(getAllUrlParams().page) + 1}" 
-            class="btn btn-sm btn-outline-secondary more" id="next">Next</a>
-`
+                `
+        }
+        if(count == 6) count = 0
 
-
-
+        }
+        document.querySelector(".prev_nex").innerHTML += `
+            <a  href="index.html?page=${
+                parseInt(getAllUrlParams().page) == 0 ? 0 : parseInt(getAllUrlParams().page) - 1}" 
+                class="btn btn-sm btn-outline-secondary more" id="prev" onclick="set_start()">Previous</a>
+            <a  href="index.html?page=${
+                parseInt(getAllUrlParams().page) * 6 >= data.length - 6 ? getAllUrlParams().page : parseInt(getAllUrlParams().page) + 1}" 
+                class="btn btn-sm btn-outline-secondary more" id="next">Next</a>
+    `
 }
 
+if(getAllUrlParams().page * 6 > data.length || getAllUrlParams().page < 0) {
+    document.body.innerHTML = 
+`
+    <div class="err_flex">
+        <div class="text"> 
+            <h1>Whoops!</h1>
+            <p>Sorry the page you are looking for does not exist</p>
+            <a href="index.html?page=0"  type="button" class="back more btn btn-sm btn-outline-secondary">Back</a>
+
+        </div>
+        <div class="img"> 
+            <img src="assets/img/err_dog.jpg" alt="">
+        </div>
+    </div>
+`
+}
+ 
 populate(getAllUrlParams().page)
 
 
